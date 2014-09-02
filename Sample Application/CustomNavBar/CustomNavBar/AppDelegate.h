dd4dd1c0d2d826baca102ba24a2fc6a81cd1fa46
admin <admin@iMac-admin.local>
2014-09-02 23:48:40 +0700

    addd custom naivagation bar

---
+++ Sample Application/CustomNavBar/CustomNavBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomNavBar
+//
+//  Created by Simon on 29/12/12.
+//  Copyright (c) 2012 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sample Application/CustomNavBar/CustomNavBar/RecipeViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecipeViewController.h
+//  CustomNavBar
+//
+//  Created by Simon on 30/12/12.
+//  Copyright (c) 2012 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeViewController : UITableViewController
+
+@end
