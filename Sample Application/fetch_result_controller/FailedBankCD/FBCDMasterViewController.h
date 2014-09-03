9d243b8e08813cc1097692761754a2b129d24a1c
admin <admin@iMac-admin.local>
2014-09-04 00:09:53 +0700

    FRC example

---
+++ Sample Application/fetch_result_controller/FailedBankCD/FBCDMasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  FBCDMasterViewController.h
+//  FailedBankCD
+//
+//  Created by Adam Burkepile on 3/23/12.
+//  Copyright (c) 2012 Adam Burkepile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBCDMasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (nonatomic,strong) NSManagedObjectContext* managedObjectContext;
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@end
