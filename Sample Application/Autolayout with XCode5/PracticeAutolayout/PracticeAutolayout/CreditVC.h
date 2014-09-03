d81f33fb41237542be1767294b9a23de1ec802be
admin <admin@iMac-admin.local>
2014-09-03 23:38:29 +0700

    auto layout

---
+++ Sample Application/Autolayout with XCode5/PracticeAutolayout/PracticeAutolayout/CreditVC.h
@@ -0,0 +1,13 @@
+//
+//  CreditVC.h
+//  PracticeAutolayout
+//
+//  Created by csnguyen on 5/21/14.
+//  Copyright (c) 2014 csnguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreditVC : UIViewController
+
+@end
---
+++ Sample Application/Autolayout with XCode5/PracticeAutolayout/PracticeAutolayout/LoadTVC.h
@@ -0,0 +1,13 @@
+//
+//  LoadTVC.h
+//  PracticeAutolayout
+//
+//  Created by csnguyen on 5/22/14.
+//  Copyright (c) 2014 csnguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadTVC : UITableViewController
+
+@end
