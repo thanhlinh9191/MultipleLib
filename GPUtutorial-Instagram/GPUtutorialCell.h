3a02c203a3f7dcc3d580651d183bbda121647efd
admin <admin@iMac-admin.local>
2014-09-07 22:59:07 +0700

    add again instagram.

---
+++ GPUtutorial-Instagram/GPUtutorialCell.h
@@ -0,0 +1,17 @@
+//
+//  GPUtutorialCell.h
+//  GPUtutorial
+//
+//  Created by Nick Barrowclough on 3/26/13.
+//  Copyright (c) 2013 iSoftware Developers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPUtutorialCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *filteredImage;
+@property (weak, nonatomic) IBOutlet UILabel *imageFilename;
+
+
+@end
---
+++ GPUtutorial-Instagram/GPUtutorialCell.m
@@ -0,0 +1,29 @@
+//
+//  GPUtutorialCell.m
+//  GPUtutorial
+//
+//  Created by Nick Barrowclough on 3/26/13.
+//  Copyright (c) 2013 iSoftware Developers. All rights reserved.
+//
+
+#import "GPUtutorialCell.h"
+
+@implementation GPUtutorialCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
